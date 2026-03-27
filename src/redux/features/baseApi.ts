e7a1f2b33889c1b3b94a0f2ca1f452844ff0cb5d
Arrafi2644 <raffian2644@gmail.com>
2026-03-27 08:52:59 +0600

    Add product management page

--- src/redux/features/baseApi.ts
+++ src/redux/features/baseApi.ts
@@ -5,7 +5,7 @@ import axiosBaseQuery from "./axiosBaseQuery";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: axiosBaseQuery(),
-  tagTypes: ["USERS", "USER", "CUSTOMERS", "ME", "BRAND", "BRANDS", "CATEGORIES", "CATEGORY", "FOODS", "FOOD", "ORDERS", "ORDER", "MY_ORDERS"],
+  tagTypes: ["USERS", "USER", "CUSTOMERS", "ME", "BRAND", "BRANDS", "CATEGORIES", "CATEGORY", "PRODUCTS", "PRODUCT", "ORDERS", "ORDER", "MY_ORDERS"],
   endpoints: () => ({}),
 
 });
---
+++ src/redux/features/product/product.api.ts
@@ -0,0 +1,70 @@
+import { baseApi } from "../baseApi";
+import type { IResponse, GetQueryParams, IPaginationMeta, IProduct } from "@/types";
+
+interface GetAllFoodsResponse {
+  success: boolean;
+  data: IProduct[];
+  meta: IPaginationMeta;
+}
+
+export const productApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+
+    // ⭐ CREATE PRODUCT
+    createProduct: builder.mutation<IResponse<IProduct>, FormData>({
+      query: (formData) => ({
+        url: "/product/create-product",
+        method: "POST",
+        data: formData,
+      }),
+      invalidatesTags: ["PRODUCTS"],
+    }),
+
+    // ⭐ UPDATE PRODUCT
+    updateProduct: builder.mutation<IResponse<IProduct>, { _id: string; formData: FormData }>({
+      query: ({ _id, formData }) => ({
+        url: `/product/${_id}`,
+        method: "PATCH",
+        data: formData,
+      }),
+      invalidatesTags: (result, error, { _id }) => ["PRODUCTS", { type: "PRODUCT", _id }],
+    }),
+
+    // ⭐ DELETE PRODUCT
+    deleteProduct: builder.mutation<IResponse<{ id: string }>, string>({
+      query: (id) => ({
+        url: `/product/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: (result, error, id) => ["PRODUCTS", { type: "PRODUCT", id }],
+    }),
+
+    // ⭐ GET SINGLE PRODUCT
+    getSingleProduct: builder.query<IResponse<IProduct>, string>({
+      query: (slug) => ({
+        url: `/product/${slug}`,
+        method: "GET",
+      }),
+      providesTags: (result, error, slug) => [{ type: "PRODUCT", id: slug }],
+    }),
+
+    // ⭐ GET ALL products
+    getAllProducts: builder.query<GetAllFoodsResponse, GetQueryParams>({
+      query: (params) => ({
+        url: "/product/all-products",
+        method: "GET",
+        params,
+      }),
+      providesTags: ["PRODUCTS"],
+    }),
+  }),
+  overrideExisting: true,
+});
+
+export const {
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useGetSingleProductQuery,
+  useDeleteProductMutation,
+  useGetAllProductsQuery
+} = productApi;
